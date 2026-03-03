d58d211eb5d1e5640f82b37986daa0879d702baf
Inyte13 <luisgardaz13@gmail.com>
2026-03-02 23:15:24 -0500

    chore: usamos el id, mas rapido

--- frontend/src/components/Actividad.tsx
+++ frontend/src/components/Actividad.tsx
@@ -2,9 +2,10 @@ import { memo } from 'react'
 import ColorPicker from './ColorPicker'
 import { useActividadesStore } from '../store/actividadesStore'
 
-export default memo(function Actividad({ actividad }: { actividad: ActividadRead}) {
   const nombreBd = actividad.nombre
   const nombre = nombreBd.charAt(0).toUpperCase() + nombreBd.slice(1)
+// TODO: El primer cambio si renderiza todos los
+export default memo(function Actividad({ id }: { id: number }) {
   return (
     <div className='flex items-center gap-2 p-1.5 hover:bg-accent hover:text-accent-foreground cursor-pointer rounded-lg'>
       <ColorPicker actividad={actividad} />
