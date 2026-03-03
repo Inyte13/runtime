90ea478c73359d11701c6e90991fc096a484c883
Inyte13 <luisgardaz13@gmail.com>
2026-03-02 23:19:00 -0500

    chore: recorremos los ids haciendolo mas rapido

--- frontend/src/components/ListaActividades.tsx
+++ frontend/src/components/ListaActividades.tsx
@@ -6,7 +6,9 @@ import { useShallow } from 'zustand/react/shallow'
 
 
 export default function ListaActividades() {
-  const actividades = useActividadesStore(state => state.actividades)
+  const actividadesIds = useActividadesStore(
+    useShallow(state => state.actividades.map(actividad => actividad.id))
+  )
   const traerActividades = useActividadesStore(state => state.traerActividades)
 
   useEffect(() => {
@@ -17,9 +19,9 @@ export default function ListaActividades() {
     <section className='flex flex-col h-full overflow-hidden p-4 gap-y-2 justify-content'>
       <div className='flex-1 min-h-0 overflow-y-auto border border-border rounded-lg bg-card text-card-foreground [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]'>
         <ul className='flex flex-col divide-y divide-border/30 px-4'>
-          {actividades.map(actividad => (
-            <li className='first:pt-2 last:pb-2' key={actividad.id}>
-              <Actividad actividad={actividad} />
+          {actividadesIds.map(id => (
+            <li className='first:pt-2 last:pb-2' key={id}>
+              <Actividad id={id} />
             </li>
           ))}
         </ul>
