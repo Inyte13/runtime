2183bd5d865e8ddb3fab37349fc6d76514443a68
Inyte13 <luisgardaz13@gmail.com>
2026-03-03 16:40:29 -0500

    chore: para salir del foco con enter

---
+++ frontend/src/utils/keboard.ts
@@ -0,0 +1,8 @@
+import { KeyboardEvent } from 'react'
+
+// Cuando pulse enter quite el blur/foco
+export function manejarEnter (e: KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) {
+  if (e.key === 'Enter') {
+    e.currentTarget.blur() 
+  }
+}
