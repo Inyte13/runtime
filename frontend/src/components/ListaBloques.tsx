459e89bdd1f469c8c64e9d62585718f36f75c90a
Inyte13 <luisgardaz13@gmail.com>
2026-03-02 23:19:36 -0500

    chore: usamos los ids para ir mas rapido

--- frontend/src/components/ListaBloques.tsx
+++ frontend/src/components/ListaBloques.tsx
@@ -15,25 +15,24 @@ import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 import { restrictToVerticalAxis } from '@dnd-kit/modifiers'
 
 export default memo(function ListaBloques() {
-  // (?.): Lo usamos porque diaDetail puede no estar creado
-  // (|| []): Lo usamos porque los bloques puede no tener bloques ([])
-  const bloques = useDiasStore(state => state.diaDetail?.bloques) || []
-  const crearBloque = useDiasStore(state => state.crearBloque)
+  const bloquesIds = useDiasStore(
+    useShallow(
+      state => state.diaDetail?.bloques?.map(bloque => bloque.id) ?? []
+    )
+  )
 
   return (
     <section className='flex flex-col h-full'>
       <div className='flex-1 min-h-0 overflow-y-auto px-4 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]'>
-        <ul className='flex flex-col gap-y-2'>
-          {bloques.map(bloque => (
-            <li key={bloque.id}>
-              <Bloque bloque={bloque} />
-            </li>
-          ))}
-        </ul>
         <Button className='w-full mt-2' onClick={crearBloque}>
           <Plus />
         </Button>
       </div>
+          <ul className='flex flex-col gap-y-2'>
+            {bloquesIds.map(id => (
+              <BloqueOrdenable key={id} id={id} />
+            ))}
+          </ul>
     </section>
   )
 })
