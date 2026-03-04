85980bc9849c13caa5e92cb36c60d76cde33c128
Inyte13 <luisgardaz13@gmail.com>
2026-03-03 21:27:24 -0500

    chore: crea el actividadtemp con el button

--- frontend/src/components/ListaActividadesFooter.tsx
+++ frontend/src/components/ListaActividadesFooter.tsx
@@ -3,23 +3,13 @@ import { Button } from './ui/button'
 import { Plus } from 'lucide-react'
 
 // Solo para no renderizas el btn cuando use el SelectorActividad
-export default memo(function ListaActividadesFooter() {
-  const crearActividad = useActividadesStore(state => state.crearActividad)
-
+export default memo(function ListaActividadesFooter({onCreate}: {onCreate: () => void}) {
   return (
     <footer className='w-full'>
       <Button
         size='icon-md'
         className='w-full'
-        onClick={
-          () =>
-            crearActividad({
-              nombre: 'Test',
-              color: '#ff0000',
-              is_active: true,
-            })
-          // TODO: Crear menu para crear actividad
-        }
+        onClick={onCreate}
       >
         <Plus />
       </Button>
