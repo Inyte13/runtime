4c6659045907a0440ff5cfaa71286a4ef1360e90
Inyte13 <luisgardaz13@gmail.com>
2026-03-04 23:34:51 -0500

    style: menos opacity

--- frontend/src/components/ListaActividades.tsx
+++ frontend/src/components/ListaActividades.tsx
@@ -20,7 +20,7 @@ export default function ListaActividades() {
   return (
     <section className='flex flex-col max-w-60 h-full overflow-hidden p-4 gap-y-2 justify-content'>
       <div className='flex-1 min-h-0 overflow-y-auto border border-border rounded-lg bg-card text-card-foreground [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]'>
-        <ul className='flex flex-col divide-y divide-border/30 px-4'>
+        <ul className='flex flex-col divide-y divide-border/50 px-4'>
           {actividadesIds.map(id => (
             <li className='first:pt-2 last:pb-2' key={id}>
               <Actividad id={id} />
