7218712632bf8a61f3c617c00fced394fcbdd470
Inyte13 <luisgardaz13@gmail.com>
2026-03-06 08:54:11 -0500

    chore: lista de actividades activas

--- frontend/src/components/Activas.tsx
+++ frontend/src/components/Activas.tsx
@@ -5,3 +5,42 @@ import { useActividadesStore } from '@/store/actividadesStore'
 import { useShallow } from 'zustand/react/shallow'
 import { memo } from 'react'
 
+export default memo(function Activas({
+  isCreate,
+  offCreate,
+}: {
+  isCreate: boolean
+  offCreate: () => void
+}) {
+  const activasIds = useActividadesStore(
+    useShallow(state =>
+      state.actividadesDetail
+        .filter(actividad => actividad.is_active)
+        .map(actividad => actividad.id)
+    )
+  )
+
+  return (
+    <>
+      {activasIds.length === 0 ? (
+        <div className='h-full flex flex-col items-center justify-center gap-1 text-muted-foreground p-4 border border-border rounded-lg bg-card'>
+          <Ghost className='size-4' />
+          <p className='text-sm'>Sin actividades aún</p>
+        </div>
+      ) : (
+        <ul className='flex flex-col divide-y divide-border/50 px-4 flex-1 overflow-y-auto border border-border rounded-lg bg-card text-card-foreground [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none][scrollbar-width:none]'>
+          {activasIds.map(id => (
+            <li className='first:pt-2 last:pb-2' key={id}>
+              <Actividad id={id} />
+            </li>
+          ))}
+          {isCreate && (
+            <li className='first:pt-2 last:pb-2'>
+              <ActividadTemp offCreate={offCreate} />
+            </li>
+          )}
+        </ul>
+      )}
+    </>
+  )
+})
