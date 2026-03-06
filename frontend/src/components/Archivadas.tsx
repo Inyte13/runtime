7924bc4b6d05e55d2cd44a69e2769480e7108220
Inyte13 <luisgardaz13@gmail.com>
2026-03-06 08:54:17 -0500

    chore: lista de actividades archivadas

--- frontend/src/components/Archivadas.tsx
+++ frontend/src/components/Archivadas.tsx
@@ -4,3 +4,30 @@ import { useShallow } from 'zustand/react/shallow'
 import Actividad from './Actividad'
 import { memo } from 'react'
 
+export default memo(function Archivadas() {
+  const archivadasIds = useActividadesStore(
+    useShallow(state =>
+      state.actividadesDetail
+        .filter(actividad => !actividad.is_active)
+        .map(actividad => actividad.id)
+    )
+  )
+  return (
+    <>
+      {archivadasIds.length === 0 ? (
+        <div className='h-full flex flex-col items-center justify-center gap-1 text-muted-foreground p-4 border border-border rounded-lg bg-card'>
+          <Archive className='size-4' />
+          <p className='text-sm'>Sin actividades archivadas</p>
+        </div>
+      ) : (
+        <ul className='flex flex-col divide-y divide-border/50 px-4 flex-1 overflow-y-auto border border-border rounded-lg bg-card text-card-foreground [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none][scrollbar-width:none]'>
+          {archivadasIds.map(id => (
+            <li className='first:pt-2 last:pb-2' key={id}>
+              <Actividad id={id} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  )
+})
