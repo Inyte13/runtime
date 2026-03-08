5bb058d1a54176e035653bcb2f6631f1ad1962f2
Inyte13 <luisgardaz13@gmail.com>
2026-03-07 20:44:06 -0500

    chore: nuevo type para bloque

--- frontend/src/types/Bloque.d.ts
+++ frontend/src/types/Bloque.d.ts
@@ -1,22 +1,21 @@
+export interface BloqueCreate {
+  duracion?: number
+  descripcion?: string | null
+  id_actividad?: number
+  fecha: string
+}
+
 export interface BloqueRead {
   id: number
   hora: string
   descripcion: string | null
   actividad: ActividadRead
-  duracion: number | null
-  hora_fin: string | null
-}
-
-export interface BloqueCreate {
-  hora?: string | null
-  descripcion?: string | null
-  id_actividad?: number | null
-  fecha: string
-  duracion?: number | null
+  duracion: number
+  hora_fin: string
 }
 
 export interface BloqueUpdate {
   descripcion?: string | null
   id_actividad?: number
-  duracion?: number | null
+  duracion?: number
 }
