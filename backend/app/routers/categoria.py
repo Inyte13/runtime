aa2aa2f7f0804b1d9ab7021552048c5cc1fa93fa
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 15:13:02 -0500

    feat: post categoria

--- backend/app/routers/categoria.py
+++ backend/app/routers/categoria.py
@@ -23,3 +23,14 @@ categoria_router = APIRouter(tags=['Categorias'])
 @categoria_router.get('/categorias', response_model=list[CategoriaReadDetail])
 def get_categorias(session: SessionDep):
   return mostrar_categorias(session)
+@categoria_router.post(
+  '/categorias', status_code=201, response_model=CategoriaReadDetail
+)
+def post_categoria(session: SessionDep, categoria: CategoriaCreate):
+  try:
+    return create_categoria(session, Categoria.model_validate(categoria))
+  # Usando el unique del schema
+  except IntegrityError:
+    raise HTTPException(
+      status_code=status.HTTP_400_BAD_REQUEST, detail='El nombre ya existe'
+    )
