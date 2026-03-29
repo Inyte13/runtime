54217abe124b2d1af64603d4ce7d49aad3a85226
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 10:09:30 -0500

    feat: id siempre puede ser none por sqlite

--- backend/app/models/categoria.py
+++ backend/app/models/categoria.py
@@ -5,3 +5,6 @@ from app.models.actividad import Actividad
 
 class Categoria(SQLModel, table=True):
   __table_args__ = {'sqlite_autoincrement': True}
+  # Tiene que ser | None, porque sqlite le asigna el id
+  # No tiene el nullable=False porque sqlite lo gestiona
+  id: int | None = Field(default=None, primary_key=True)
