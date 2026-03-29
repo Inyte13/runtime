c545f2a2240fecd60b9875e7eec527e5392efd82
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 14:00:21 -0500

    chore: bloque create

--- backend/app/schemas/bloque.py
+++ backend/app/schemas/bloque.py
@@ -4,3 +4,13 @@ from pydantic import field_serializer, field_validator
 from sqlmodel import Field, SQLModel
 
 
+# Necesita menos indicaciones porque no tiene tabla
+class BloqueCreate(SQLModel):
+  duracion: float = 0.5
+  # TODO: Validator para que envie mas del multiplo de 0.5 o lo que escoja
+  # Aquí si va porque es validación de datos, no indicaciones para la bd
+  descripcion: str | None = Field(default=None, max_length=255)
+  fecha: date
+  # Podemos recibir None pero lo controlaremos en el backend
+  id_actividad: int | None = None
+  id_ref: int | None = None
