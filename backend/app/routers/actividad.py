e4771cf9920588999482a2bb254766a9df6fd6c2
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 15:15:04 -0500

    feat: post actividad validando el unique del schema

--- backend/app/routers/actividad.py
+++ backend/app/routers/actividad.py
@@ -18,3 +18,14 @@ from app.services.actividad import (
 actividad_router = APIRouter(tags=['Actividades'])
 
 
+@actividad_router.post(
+  '/actividades', status_code=201, response_model=ActividadReadDetail
+)
+def post_actividad(session: SessionDep, actividad: ActividadCreate):
+  try:
+    return registrar_actividad(session, actividad)
+  # Usando el unique del schema
+  except IntegrityError:
+    raise HTTPException(
+      status_code=status.HTTP_400_BAD_REQUEST, detail='El nombre ya existe'
+    )
