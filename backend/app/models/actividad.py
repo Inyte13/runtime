406c3007fd9e445f238e23989322989fc4cb53a6
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 10:11:47 -0500

    feat: quitamos color que lo lleva categoria y lo relacionamos

--- backend/app/models/actividad.py
+++ backend/app/models/actividad.py
@@ -6,6 +6,7 @@ class Actividad(SQLModel, table=True):
   # Tiene que ser | None, porque sqlite le asigna el id
   # No tiene el nullable=False porque sqlite lo gestiona
   id: int | None = Field(default=None, primary_key=True)
-  color: str = Field(max_length=7, default='#A18072', nullable=False)
   nombre: str = Field(unique=True, max_length=50, nullable=False)
   is_active: bool = Field(default=True, nullable=False)
+  # Tenemos id_actividad a pesar de tener actividad porque es FK
+  id_categoria: int = Field(foreign_key='categoria.id', nullable=False)
