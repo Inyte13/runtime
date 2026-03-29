9acc33d9daa3cfb6292aa6a7d89b8b4544a75452
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 15:19:57 -0500

    feat: modificar hora validando que no se pase de la medianoche

--- backend/app/utils/time.py
+++ backend/app/utils/time.py
@@ -1,3 +1,12 @@
 from datetime import time
 
 
+def modificar_hora(hora: time, duracion: float) -> time:
+  # Calculamos pero en minutos, por las 24:00
+  minutos_inicio = hora.hour * 60 + hora.minute
+  minutos_fin = minutos_inicio + int(duracion * 60)
+  if minutos_fin == 1440:
+    return time(0, 0)
+  if minutos_fin > 1440:
+    raise ValueError('El bloque se pasa de medianoche')
+  return time(minutos_fin // 60, minutos_fin % 60)
