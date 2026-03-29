ea9a2e5de2175952c0ce460ae1b51b0726fe10a9
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 10:11:26 -0500

    feat: no busco por nombre, eso lo hace el frontend

--- backend/app/models/actividad.py
+++ backend/app/models/actividad.py
@@ -6,6 +6,6 @@ class Actividad(SQLModel, table=True):
   # Tiene que ser | None, porque sqlite le asigna el id
   # No tiene el nullable=False porque sqlite lo gestiona
   id: int | None = Field(default=None, primary_key=True)
-  nombre: str = Field(index=True, unique=True, max_length=50, nullable=False)
   color: str = Field(max_length=7, default='#A18072', nullable=False)
+  nombre: str = Field(unique=True, max_length=50, nullable=False)
   is_active: bool = Field(default=True, nullable=False)
