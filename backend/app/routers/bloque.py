d1c3ed5a4f3ba782fa59d785a3d9613dfa8c1c21
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 15:14:16 -0500

    feat: post bloques

--- backend/app/routers/bloque.py
+++ backend/app/routers/bloque.py
@@ -13,3 +13,9 @@ bloque_router = APIRouter(tags=['Bloques'])
 
 
 # TODO: Un get para traer los bloques para las estadisticas
+@bloque_router.post('/bloques', status_code=201, response_model=BloqueRead)
+def post_bloque(session: SessionDep, bloque: BloqueCreate):
+  try:
+    return registrar_bloque(session, bloque)
+  except ValueError as e:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
