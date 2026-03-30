7762f5afe7148b12afac4564a7c91893f8072cd1
Inyte13 <luisgardaz13@gmail.com>
2026-03-30 10:52:45 -0500

    chore: usamos el manejarSelector automaticamente si solo hay una respuesta

--- frontend/src/components/ComboboxCategoria.tsx
+++ frontend/src/components/ComboboxCategoria.tsx
@@ -15,6 +15,23 @@ import { memo, useCallback, useEffect, useMemo, useState } from 'react'
 import ComboboxActividad from './ComboboxActividad.js'
 
 export default memo(function ComboboxCategoria({ id }: { id: number }) {
+  // Es null al limpiar con el button X
+  const manejarSelector = useCallback(
+    async (idStr: string | null) => {
+      if (!idStr) return
+      await actualizarBloque(id, { id_actividad: parseInt(idStr) })
+    },
+    [id, actualizarBloque]
+  )
+  // Si hay solo una actividad en la búsqueda, la usa
+  useEffect(() => {
+    const actividadesFiltradas = categoriasFiltradas.flatMap(
+      cat => cat.actividades
+    )
+    if (actividadesFiltradas.length === 1) {
+      manejarSelector(actividadesFiltradas[0].id.toString())
+    }
+  }, [categoriasFiltradas, manejarSelector])
   return (
     <Combobox
       items={items}
