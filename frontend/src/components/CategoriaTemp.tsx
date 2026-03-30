f7c8271a93c854a90d1623795b8ede8f480fc236
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 22:11:35 -0500

    chore: categoria temp return div

--- frontend/src/components/CategoriaTemp.tsx
+++ frontend/src/components/CategoriaTemp.tsx
@@ -5,4 +5,11 @@ import { Button } from './ui/button'
 import { Plus } from 'lucide-react'
 import { manejarEnter } from '@/utils/keyboard'
 
+export default function CategoriaTemp({
   // Si cambias el #A18072, cambialo en el backend (categoria.py) models y schemas
+  return (
+    <div
+    >
+    </div>
+  )
+}
