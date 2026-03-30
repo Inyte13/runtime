0f545df704598ace54065daf1e6f15ed8df8a11f
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 21:25:18 -0500

    chore: return sidebar section

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -3,3 +3,13 @@ import { Archive, CircleCheck } from 'lucide-react'
 import ListaCategorias from './ListaCategorias'
 import ListaArchivadas from './ListaArchivadas'
 
+export default function Sidebar() {
+  // TODO: Crear el button inteligente para contraer y ampliar las categorias
+  // TODO: Que el TabTrigger sea responsive con su contenido
+  return (
+    <Tabs defaultValue='activas' asChild>
+      <section className='flex flex-col h-full overflow-hidden gap-y-2 pl-4 w-56'>
+      </section>
+    </Tabs>
+  )
+}
