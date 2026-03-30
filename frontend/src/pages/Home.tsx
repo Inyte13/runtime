14027d82f242bcd14c189a6b8844c1d61bf8ef63
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 21:11:25 -0500

    chore: reemplazamos la mediaquery por panel manual y esta integrado dentro de layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,11 +2,9 @@ import Layout from '../components/Layout'
 import Dia from '../components/Dia'
 
 export default function Home() {
-  const isSmall = useMediaQuery(1300)
   return (
-    <main className='flex h-dvh overflow-hidden p-4 justify-center'>
-      {!isSmall && <ListaActividades />}
-      <Calendario />
+    <main className='flex h-dvh overflow-hidden justify-center'>
+      <Layout />
       <Dia />
     </main>
   )
