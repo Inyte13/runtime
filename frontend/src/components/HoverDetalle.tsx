281ad5255f0603025c81f9cabb8f9667cae66d66
Inyte13 <luisgardaz13@gmail.com>
2026-03-30 10:16:00 -0500

    chore: HoverDetalle return li

--- frontend/src/components/HoverDetalle.tsx
+++ frontend/src/components/HoverDetalle.tsx
@@ -1,3 +1,9 @@
 import { useCategoriasStore } from '@/store/categoriasStore'
 import { useDiasStore } from '@/store/diasStore'
 
+export default function HoverDetalle({
+  return (
+    <li className='flex flex-col justify-center'>
+    </li>
+  )
+}
