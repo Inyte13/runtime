f7311a17d0aa6eec918446b41e58a6369fbee6a7
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 21:29:46 -0500

    chore: state para saber si creamos y asi mostrar la categoria temp

--- frontend/src/components/ListaCategorias.tsx
+++ frontend/src/components/ListaCategorias.tsx
@@ -8,12 +8,17 @@ import { Button } from './ui/button'
 export default function ListaCategorias() {
   const ids = useCategoriasStore(state => state.categoriasDetail).map(
     categoriaDetail => categoriaDetail.id
+  const [crearCategoria, setCrearCategoria] = useState(false)
+  // TODO: Drag and drop para ordenar
   return (
     <section className='m-0 flex flex-col min-h-0 gap-y-0 items-end'>
       <section className='flex flex-col gap-y-1 overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]'>
         {ids.map(id => (
           <Categoria key={id} id={id} />
         ))}
+        {crearCategoria && (
+          <CategoriaTemp setCrearCategoria={setCrearCategoria} />
+        )}
       </section>
     </section>
   )
