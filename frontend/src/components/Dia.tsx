8471cbc30100876ce01732ee1e51b8302d5972f8
Inyte13 <luisgardaz13@gmail.com>
2026-03-30 10:20:34 -0500

    chore: TODO completado y menor ruido

--- frontend/src/components/Dia.tsx
+++ frontend/src/components/Dia.tsx
@@ -10,13 +10,10 @@ export default function Dia() {
   useEffect(() => {
     traerDiaDetail()
   }, [fecha, traerDiaDetail])
-  // TODO: width no fijo, truncate para selector y en el combobox
   return (
-    <section className='flex flex-col min-w-75 h-full gap-y-2 p-4 pr-0'>
+    <section className='flex flex-col min-w-75 h-full gap-y-2 p-4'>
       <DiaHeader />
-      <div className='flex-1 min-h-0'>
-        <ListaBloques />
-      </div>
+      <ListaBloques />
     </section>
   )
 }
