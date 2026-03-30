ae4415bc4b7166d4c20c58f207188cd330ce76d2
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 22:13:02 -0500

    chore: ref para saber al manejarBlur no se debe renderizar

--- frontend/src/components/CategoriaTemp.tsx
+++ frontend/src/components/CategoriaTemp.tsx
@@ -6,9 +6,19 @@ import { Plus } from 'lucide-react'
 import { manejarEnter } from '@/utils/keyboard'
 
 export default function CategoriaTemp({
+  const tempRef = useRef<HTMLDivElement>(null)
+  const manejarBlur = (e: React.FocusEvent<HTMLDivElement>) => {
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      actualizar()
+    }
+  }
   // Si cambias el #A18072, cambialo en el backend (categoria.py) models y schemas
   return (
     <div
+      ref={tempRef}
+      onBlur={manejarBlur}
+      tabIndex={0} // Para que sea focusable
+      className='flex items-center p-1.5 pl-2 bg-card rounded-lg'
     >
     </div>
   )
