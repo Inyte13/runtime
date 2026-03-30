c65076d19426a8a867b37c411cf9db6efd5359ba
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 22:28:37 -0500

    chore: no usamos auto scroll lo tiene categoria temp y tampoco tenemos el span de color

--- frontend/src/components/ActividadTemp.tsx
+++ frontend/src/components/ActividadTemp.tsx
@@ -13,15 +13,7 @@ export default function ActividadTemp({
   const tempRef = useRef<HTMLDivElement>(null)
 
   const [nombreTemp, setNombreTemp] = useState('')
-  // Si lo cambias también lo tienes que hacer en el backend (actividad.py)
-  const [colorTemp, setColorTemp] = useState('#A18072')
 
-  // Auto-scroll cuando se crea el holograma de Actividad
-  useEffect(() => {
-    tempRef.current?.scrollIntoView({ behavior: 'auto', block: 'end' })
-  }, [])
-
-  const actualizar = async () => {
     const nombre = nombreTemp.trim()
     if (nombre) {
       await crearActividad({ nombre: nombre.toLowerCase(), color: colorTemp })
@@ -42,21 +34,6 @@ export default function ActividadTemp({
       onBlur={manejarBlur}
       className='flex items-center p-1.5 hover:bg-accent hover:text-accent-foreground rounded-lg'
     >
-      <input
-        style={{ background: colorTemp }}
-        className={`rounded-full size-4 shrink-0 cursor-pointer p-0 
-        [&::-webkit-color-swatch-wrapper]:p-0
-        [&::-webkit-color-swatch]:opacity-0 
-        [&::-webkit-color-swatch]:border-none 
-        [&::-webkit-color-swatch]:rounded-full
-        [&::-moz-color-swatch]:border-none 
-        [&::-moz-color-swatch]:rounded-full
-        [&::-moz-color-swatch]:opacity-0
-        `}
-        type='color'
-        value={colorTemp}
-        onChange={e => setColorTemp(e.target.value)}
-      />
       <Input
         autoFocus
         className='capitalize p-0 pl-2 border-none outline-none rounded-none h-[1.6rem] text-base'
