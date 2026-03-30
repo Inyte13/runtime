3f3a3b18297fba5346bb26e103542a6c91bae8df
Inyte13 <luisgardaz13@gmail.com>
2026-03-30 11:09:43 -0500

    chore: ComboboxActividad return ComboboxItem

--- frontend/src/components/ComboboxActividad.tsx
+++ frontend/src/components/ComboboxActividad.tsx
@@ -2,3 +2,9 @@ import { useCategoriasStore } from '@/store/categoriasStore'
 import { ComboboxItem } from './ui/combobox'
 import { useColorStore } from '@/store/colorStore'
 
+export default function ComboboxActividad({
+  return (
+    <ComboboxItem
+    </ComboboxItem>
+  )
+}
