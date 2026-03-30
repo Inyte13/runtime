5110f2a729a9159d0f2cb2e1075c011be66d65ff
Inyte13 <luisgardaz13@gmail.com>
2026-03-30 10:17:58 -0500

    chore: sacamos actividad y actividadResumen con fast fail para el nombre y todo lo demas del resumen

--- frontend/src/components/HoverDetalle.tsx
+++ frontend/src/components/HoverDetalle.tsx
@@ -2,8 +2,35 @@ import { useCategoriasStore } from '@/store/categoriasStore'
 import { useDiasStore } from '@/store/diasStore'
 
 export default function HoverDetalle({
+  const actividad = useCategoriasStore(state =>
+    state.categoriasDetail
+      .find(categoria => categoria.id === idCategoria)
+      ?.actividades.find(actividad => actividad.id === idActividad)
+  )
+  const actividadResumen = useDiasStore(state =>
+    state.diasResumen.find(dia => dia.fecha === fechaISO)
+  )
+    ?.categorias.find(categoria => categoria.id === idCategoria)
+    ?.actividades.find(actividad => actividad.id === idActividad)
+  if (!actividad || !actividadResumen) return
   return (
     <li className='flex flex-col justify-center'>
+      <div className='flex justify-between gap-x-1.5 text-sm'>
+        <span className='truncate'>{actividad.nombre}</span>
+        <div className='flex justify-center items-center bg-secondary py-0.5 px-1.5 rounded-md'>
+          <span>{actividadResumen.duracion}h</span>
+        </div>
+      </div>
+      {actividadResumen.descripciones &&
+        actividadResumen.descripciones.length > 0 && (
+          <ul>
+            {actividadResumen.descripciones.map((descripcion, i) => (
+              <li className='text-secondary-foreground truncate' key={i}>
+                {descripcion}
+              </li>
+            ))}
+          </ul>
+        )}
     </li>
   )
 }
