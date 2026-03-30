a3c2878558af0389a66245fefe975f13f964ee33
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 21:59:06 -0500

    chore: sacamos categoria con el id, fail fast y pasamos la id de cada categoria

--- frontend/src/components/ListaActividades.tsx
+++ frontend/src/components/ListaActividades.tsx
@@ -4,7 +4,13 @@ import Actividad from './Actividad'
 import ActividadTemp from './ActividadTemp'
 
 export default function ListaActividades() {
+  const categoria = useCategoriasStore(state =>
+    state.categoriasDetail.find(categoria => categoria?.id === id)
+  )
+  if (!categoria) return
   return (
+      {categoria.actividades.map(actividad => (
+      ))}
           />
   )
 }
