601d8630a3aec3ffbe87a6ceaea34a5d2897a04a
Inyte13 <luisgardaz13@gmail.com>
2026-03-30 11:11:34 -0500

    chore: sacamos categoria para el color

--- frontend/src/components/ComboboxActividad.tsx
+++ frontend/src/components/ComboboxActividad.tsx
@@ -3,16 +3,26 @@ import { ComboboxItem } from './ui/combobox'
 import { useColorStore } from '@/store/colorStore'
 
 export default function ComboboxActividad({
+  const categoria = useCategoriasStore(state =>
+    state.categoriasDetail.find(categoria => categoria.id === idCategoria)
+  )
   const actividad = useCategoriasStore(state =>
     state.categoriasDetail
       .find(categoria => categoria.id === idCategoria)
       ?.actividades.find(actividad => actividad.id === idActividad)
   )
+  const color = useColorStore(
+    state => state.colores[idCategoria] 
+  )
+  if (!actividad || !categoria) return
   return (
     <ComboboxItem
       key={actividad.id}
       value={actividad.id.toString()}
       disabled={!actividad.is_active}
+      className='py-1.25 border-l border-border rounded-l-none last:rounded-bl nth-[2]:rounded-tl pl-3 '
+      style={{ borderLeftColor: color || categoria.color}}
+    >
       <span className='capitalize truncate'>{actividad.nombre}</span>
     </ComboboxItem>
   )
