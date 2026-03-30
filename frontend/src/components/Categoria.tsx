fd62361fd839fbf73dd6b5a68eb1c4ebc9bbda44
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 21:42:09 -0500

    chore: sacamos categoria para obtener actividades y usamos fail fast

--- frontend/src/components/Categoria.tsx
+++ frontend/src/components/Categoria.tsx
@@ -11,8 +11,14 @@ import { ChevronDown, Plus } from 'lucide-react'
 import ListaActividades from './ListaActividades'
 
 export default function Categoria({ id }: { id: number }) {
+  const categoria = useCategoriasStore(state =>
+    state.categoriasDetail.find(categoria => categoria.id === id)
+  )
+  if (!categoria) return
   return (
     <Collapsible open={abrir} onOpenChange={setAbrir} asChild>
+          {categoria.actividades.length > 0 && (
+          )}
     </Collapsible>
   )
 }
