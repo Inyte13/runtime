dad3d09af11dcd1995e856b9cc41f0898e9f624f
Inyte13 <luisgardaz13@gmail.com>
2026-03-29 21:59:25 -0500

    chore: lista actividades return ul

--- frontend/src/components/ListaActividades.tsx
+++ frontend/src/components/ListaActividades.tsx
@@ -3,14 +3,20 @@ import { useColorStore } from '@/store/colorStore'
 import Actividad from './Actividad'
 import ActividadTemp from './ActividadTemp'
 
-export default function ListaActividades() {
+export default function ListaActividades({
   const categoria = useCategoriasStore(state =>
     state.categoriasDetail.find(categoria => categoria?.id === id)
   )
   if (!categoria) return
   return (
+    <ul
+      className='flex flex-col divide-y divide-border/50 border-l-2 border-border rounded-lg bg-card/50'
+      style={{ borderLeftColor: color || categoria.color }}
+    >
       {categoria.actividades.map(actividad => (
       ))}
           />
+      )}
+    </ul>
   )
 }
