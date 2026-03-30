e156449181b6b0ecbf79980fbfc68ca08c0a7ff1
Inyte13 <luisgardaz13@gmail.com>
2026-03-30 11:10:49 -0500

    chore: sacamos actividad para obtener sus values

--- frontend/src/components/ComboboxActividad.tsx
+++ frontend/src/components/ComboboxActividad.tsx
@@ -3,8 +3,17 @@ import { ComboboxItem } from './ui/combobox'
 import { useColorStore } from '@/store/colorStore'
 
 export default function ComboboxActividad({
+  const actividad = useCategoriasStore(state =>
+    state.categoriasDetail
+      .find(categoria => categoria.id === idCategoria)
+      ?.actividades.find(actividad => actividad.id === idActividad)
+  )
   return (
     <ComboboxItem
+      key={actividad.id}
+      value={actividad.id.toString()}
+      disabled={!actividad.is_active}
+      <span className='capitalize truncate'>{actividad.nombre}</span>
     </ComboboxItem>
   )
 }
