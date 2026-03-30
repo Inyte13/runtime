bc30eeee7bd3dfb3ac60b7066b9de7f6087d743c
Inyte13 <luisgardaz13@gmail.com>
2026-03-30 10:18:12 -0500

    chore: params para buscar de nuevo

--- frontend/src/components/HoverDetalle.tsx
+++ frontend/src/components/HoverDetalle.tsx
@@ -2,6 +2,14 @@ import { useCategoriasStore } from '@/store/categoriasStore'
 import { useDiasStore } from '@/store/diasStore'
 
 export default function HoverDetalle({
+  fechaISO,
+  idActividad,
+  idCategoria,
+}: {
+  fechaISO: string
+  idActividad: number
+  idCategoria: number
+}) {
   const actividad = useCategoriasStore(state =>
     state.categoriasDetail
       .find(categoria => categoria.id === idCategoria)
